2465d9341a654d0177c49dbfd6f2327901e9cf6f
Alam Arias <Alam.GBC@gmail.com>
2017-03-16 15:17:25 -0400

    check: add error checking

--- check.py
+++ check.py
@@ -2,11 +2,19 @@
 import csv
 import sys
 
-for i in sys.argv:
-    with open(i, newline='') as JP:
-        JPCSV = list(csv.reader(JP))
-        WC = open(i.replace("/JP/","/WC/"),newline='')
-        WCCSV = list(csv.reader(WC))
+for i in sys.argv[1:]:
+    with open(i) as JP:
+        try:
+            JPCSV = list(csv.reader(JP,strict=True))
+        except:
+            print("Error reading {0}".format(i))
+            break;
+        WC = open(i.replace("/JP/","/WC/"))
+        try:
+            WCCSV = list(csv.reader(WC,strict=True))
+        except:
+             print("Error reading WC vresion of {0}".format(i))
+             break;
         WClist = []
         for row in WCCSV:
             for n, col in enumerate(row):
