0e396f15ca2ef8afdace7f360b22108244f4f3cc
Alam Arias <Alam.GBC@gmail.com>
2017-03-16 19:29:58 -0400

    check: fixup for missing files

--- check.py
+++ check.py
@@ -11,31 +11,31 @@ if len(sys.argv) == 1:
 
 for i in sys.argv[1:]:
     with open(i) as JP:
+
         try:
             JPCSV = list(csv.reader(JP,strict=True))
         except:
             print("Error reading {0}".format(i))
             sys.exit(os.EX_DATAERR)
             break;
-        WC = open(i.replace("/JP/","/WC/"))
+
         try:
-            WCCSV = list(csv.reader(WC,strict=True))
+            WC = open(i.replace("/JP/","/WC/"))
         except:
-             print("Error reading WC version of {0}".format(i))
-             sys.exit(os.EX_UNAVAILABLE)
-             break;
+            print("Error reading WC version of {0}".format(i))
+            sys.exit(os.EX_UNAVAILABLE)
+            break;
+        WCCSV = list(csv.reader(WC,strict=True))
         WClist = []
         for row in WCCSV:
             for n, col in enumerate(row):
                 if n == 0:
                      WClist.append(col)
-                     #print(col)
-        #print(WClist)
         for row in JPCSV:
             for n, col in enumerate(row):
                 if n == 0:
                      if col not in WClist:
                          print("File {} have extra line: {}".format(i, col))
-                         err =  os.EX_NOTFOUND
+                         err =  1
 
 sys.exit(err)
