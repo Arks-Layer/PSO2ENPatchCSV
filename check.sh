00232c3ece2b62e519c625fc7f4333684a27d1d3
Alam Arias <Alam.GBC@gmail.com>
2017-03-17 00:18:23 -0400

    check: fixup script to handle lang branch

--- check.sh
+++ check.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf /tmp/PSO2ENPatchCSV
-git archive --format=tar --prefix=PSO2ENPatchCSV/JP/ origin/JP | (cd /tmp && tar xf -)
-git archive --format=tar --prefix=PSO2ENPatchCSV/WC/ HEAD | (cd /tmp && tar xf -)
-find /tmp/PSO2ENPatchCSV/JP/ -name "*.csv" -print0|xargs -0 python3 check.py
+git archive --format=tar --prefix=PSO2ENPatchCSV/JP/ origin/JP | tar xf - -C /tmp
+rsync -a . /tmp/PSO2ENPatchCSV/WC --exclude ".git" --del
+cd /tmp/PSO2ENPatchCSV/WC/Files
+find .. -name "*.csv" -not -path "../Files/*" -exec ln -s {} . \;
+cd ..
+find /tmp/PSO2ENPatchCSV/JP/Files -name "*.csv" -print0|xargs -0 python3 check.py
