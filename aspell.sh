8ab719613a86cc1d24a563ef0f57161ea855035f
Alam Arias <Alam.GBC@gmail.com>
2017-04-26 22:40:43 -0400

    CircleCI: move reject list for dict building to a file

--- aspell.sh
+++ aspell.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cat PSO2.dict.BASE>/tmp/PSO2.dict
-find . -name "*_name.csv" -name "ui_charamake_parts.csv" -print0|xargs -0 python3 aspell.py|strings -n 2|sed -e 's/[ \t][ \t]*/\
-/g' -e 's/\!//g' -e "s/'//g"|uniq|grep -v -e "weather0" -e "poka1" -e "poka2" -e "Ship0" -e "Ship1" -e "Self0" -e "PSO2es" -e "PSO2" -e "0%"|strings -n 1 >>/tmp/PSO2.dict
+find . -name "*_name.csv" -or -name "ui_charamake_parts.csv" -print0|xargs -0 python3 aspell.py|strings -n 2|sed -e 's/[ \t][ \t]*/\
+/g' -e 's/\!//g' -e "s/'//g"|uniq|grep -v -E -f reject*.dict |strings -n 1 >>/tmp/PSO2.dict
 cat PSO2.*.dict|strings -n 1 >>/tmp/PSO2.dict
 find . -name "*.csv" -not -name "smut_filter.csv" -not -name "*_BACKUP_*.csv" -not -name "*_BASE_*.csv" -not -name "*_REMOTE_*.csv" -print0|xargs -0 python3 aspell.py|strings -n 1|aspell pipe --personal=/tmp/PSO2.dict --encoding utf-8|strings -n 2|fgrep -v -e "*"
 #./aspell.sh |tee /tmp/PSO2_aspell.txt|sort|uniq --count|sort -n|2>/tmp/PSO2_aout.txt
