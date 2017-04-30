f6a81665fba9e97c8315a60a566b96f71919cf43
Alam Arias <Alam.GBC@gmail.com>
2017-04-29 22:50:28 -0400

    CircleCI: move quote.py

--- _scripts/quote.sh
+++ _scripts/quote.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -name "*.csv" -print0|xargs -0 python3 check.py&>/dev/null||find . -name "*.csv" -print0|xargs -0 -n 1 python3 check.py
+find . -name "*.csv" -print0|xargs -0 python3 _py/quote.py&>/dev/null||find . -name "*.csv" -print0|xargs -0 -n 1 python3 _py/quote.py
