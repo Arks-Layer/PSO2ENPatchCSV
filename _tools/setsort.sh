b10c02b7388dd24a3d7e8b2b7e18a673b61923d8
Alam Arias <Alam.GBC@gmail.com>
2017-05-03 02:54:14 -0400

    JP: fixup sort scripts some more

--- _tools/setsort.sh
+++ _tools/setsort.sh
@@ -3,12 +3,12 @@ while IFS=' ' read -r file dir
 do
 	newdir=`echo $dir|sed "s/\"//g"`
 	if [ ! -d "$newdir" ]; then
-		echo "Making directory " $newdir
+		#echo "Making directory $newdir"
 		mkdir -p "$newdir"
 	fi
 	newfile=`find  . -not -path "./Files/*" -not -path "*/.git/*" -name "$file"`
 	if [ ! -z "$newfile" ]; then
-		echo "Moving $newfile to $newdir"
-		#git mv -f $newfile "$newdir" 2>/dev/null
+		#echo "Moving $newfile to $newdir"
+		git mv -f $newfile "$newdir" 2>/dev/null
 	fi
 done
