c9e2229a3333ae1a51c4459802c92237afa85a89
Alam Arias <Alam.GBC@gmail.com>
2017-05-03 01:55:20 -0400

    tools: support ash for setsort.sh

--- _tools/setsort.sh
+++ _tools/setsort.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 while IFS=' ' read -r file dir
 do
 	mkdir -p $dir
-	newfile="$(find  . -name "*.csv" -not -path "../Files/*" -not -path "*/.git/*" -name "$file")"
+	newfile=`find  . -name "*.csv" -not -path "../Files/*" -not -path "*/.git/*" -name "$file"`
 	git mv $newfile $dir
 done
