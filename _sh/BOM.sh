db0a6ff159739b0f5b6e170d395804b5f35612b0
Alam Arias <Alam.GBC@gmail.com>
2017-05-02 20:32:46 -0400

    CircleCI: use less resources

--- _sh/BOM.sh
+++ _sh/BOM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm /tmp/BOM.stderr
-find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*" -print0|xargs -P 0 -n 1024 -0 ./_py/BOM.py 2> /tmp/BOM.stderr
+find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*" -print0|_tools/mp.sh -0 ./_py/BOM.py 2> /tmp/BOM.stderr
 if [ -s /tmp/BOM.stderr ]; then
 	cat /tmp/BOM.stderr
 	exit 1
--- _sh/quote.sh
+++ _sh/quote.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -name "*.csv" -not -path "*/.git/*" -print0|xargs -P 0 -n 1024 -0 _py/quote.py&>/dev/null||find . -name "*.csv" -not -path "*/.git/*" -print0|xargs -P 0 -0 -n 1 _py/quote.py
+find . -name "*.csv" -not -path "*/.git/*" -print0|_tools/mp.sh -0 _py/quote.py&>/dev/null||find . -name "*.csv" -not -path "*/.git/*" -print0|_tools/mp.sh -0 -n 1 _py/quote.py
