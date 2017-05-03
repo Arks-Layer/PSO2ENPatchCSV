87edf94a237c0adb7aa22a66be81acc3032acbe3
Alam Arias <Alam.GBC@gmail.com>
2017-05-03 02:49:27 -0400

    JP: fixup sort scripts

--- _tools/getsort.sh
+++ _tools/getsort.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 rm -rf /tmp/PSO2ENPatchCSV.sort
 if [ "$#" -ne 1 ]; then
-	git archive --format=tar --prefix=PSO2ENPatchCSV.sort/ HEAD | tar xf - -C /tmp
+	git archive --format=tar --prefix=PSO2ENPatchCSV.sort/ origin/EN | tar xf - -C /tmp
 else
 	git archive --format=tar --prefix=PSO2ENPatchCSV.sort/ $1 | tar xf - -C /tmp
 fi
 cd /tmp/PSO2ENPatchCSV.sort
-find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*"|rev|sed 's/\//\t/'|rev|awk -F'\t' ' { t = $1; $1 = $2; $2 = t; printf("%s \"%s\"\n", $1, $2); } '
+find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*"|rev|sed 's/\//\t/'|rev|sed 's/\.\///g'|awk -F'\t' ' { t = $1; $1 = $2; $2 = t; printf("%s \"%s\"\n", $1, $2); } '
 rm -rf /tmp/PSO2ENPatchCSV.sort
--- _tools/setsort.sh
+++ _tools/setsort.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 while IFS=' ' read -r file dir
 do
-	mkdir -p $dir
-	newfile=`find  . -name "*.csv" -not -path "../Files/*" -not -path "*/.git/*" -name "$file"`
-	git mv $newfile $dir
+	newdir=`echo $dir|sed "s/\"//g"`
+	if [ ! -d "$newdir" ]; then
+		echo "Making directory " $newdir
+		mkdir -p "$newdir"
+	fi
+	newfile=`find  . -not -path "./Files/*" -not -path "*/.git/*" -name "$file"`
+	if [ ! -z "$newfile" ]; then
+		echo "Moving $newfile to $newdir"
+		#git mv -f $newfile "$newdir" 2>/dev/null
+	fi
 done
