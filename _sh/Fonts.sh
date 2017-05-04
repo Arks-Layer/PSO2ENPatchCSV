70b767eb234993597d142dd499ebdd3ae3b90030
Alam Arias <Alam.GBC@gmail.com>
2017-05-04 01:28:13 -0400

    Scripts: retry font check

--- _sh/Fonts.sh
+++ _sh/Fonts.sh
@@ -2,4 +2,4 @@
 rm -rf /tmp/PSO2ENPatchCSV.Fonts
 rsync -a . /tmp/PSO2ENPatchCSV.Fonts --exclude ".git" --del
 cd /tmp/PSO2ENPatchCSV.Fonts
-find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*" -print0|_tools/mp.sh -0 ./_py/Fonts.py
+find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*" -print0|./_tools/mp.sh -0 ./_py/Fonts.py
