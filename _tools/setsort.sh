d72710abe00975bf599b2b090657f4b771efcc46
Alam Arias <Alam.GBC@gmail.com>
2017-05-07 11:44:08 -0400

    Tools: add support to move the transalte files to a subfolder

--- _tools/setsort.sh
+++ _tools/setsort.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+if [ "$#" -ne 1 ]; then
+	basedir=""
+else
+	basedir="$1"
+fi
 while IFS=' ' read -r file dir
 do
 	newdir=`echo $dir|sed "s/\"//g"`
-	if [ ! -d "$newdir" ]; then
+	if [ ! -d "$basedir$newdir" ]; then
 		#echo "Making directory $newdir"
-		mkdir -p "$newdir"
+		mkdir -p "$basdir$newdir"
 	fi
 	newfile=`find  . -not -path "./Files/*" -not -path "*/.git/*" -name "$file"`
 	if [ ! -z "$newfile" ]; then
 		#echo "Moving $newfile to $newdir"
-		git mv -f $newfile "$newdir" 2>/dev/null
+		git mv -f $newfile "$basedir$newdir" 2>/dev/null
 	fi
 done
