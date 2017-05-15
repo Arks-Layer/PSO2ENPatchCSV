d5da1d211a762aca593bbade8782c11c8c24086c
Alam Arias <Alam.GBC@gmail.com>
2017-05-14 20:29:03 -0400

    Scripts: check with upstream's JP

--- _sh/release.sh
+++ _sh/release.sh
@@ -2,7 +2,7 @@
 if [ -f release.sed ]; then
 	find . -name "*.csv" -not -path "../Files/*" -not -path "*/.git/*" -print0|_tools/mp.sh -0 sed -i'' -f release.sed
 fi
-git show origin/EN:release.sed > /tmp/release.sed
+git show upstream/EN:release.sed > /tmp/release.sed
 if [ -s  /tmp/release.sed ]; then
 	find . -name "*.csv" -not -path "../Files/*" -not -path "*/.git/*" -print0|_tools/mp.sh -0 sed -i'' -f /tmp/release.sed
 fi
--- _tools/TODO.sh
+++ _tools/TODO.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-find . -path "./Files/*" -print0|_tools/mp.sh -0 git diff --name-status --exit-code origin/JP --|sed -e "s/M\t//g"|xargs -n 1 -I{} -- git mv {} Files.TODO
+find . -path "./Files/*" -print0|_tools/mp.sh -0 git diff --name-status --exit-code upstream/JP --|sed -e "s/M\t//g"|xargs -n 1 -I{} -- git mv {} Files.TODO
