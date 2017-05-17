50a443a4f961716c22cdf1eb1bce3eee94daaa83
Alam Arias <Alam.GBC@gmail.com>
2017-05-17 19:11:28 -0400

    MP: do up to 2 jobs

--- _tools/mp.sh
+++ _tools/mp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #xargs --max-procs 8 --no-run-if-empty $0
-parallel --max-procs 1 --no-run-if-empty --progress --xargs $@
+parallel --max-procs 2 --no-run-if-empty --progress --xargs -m $@
