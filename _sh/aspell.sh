802bf6e96649b39741985c6f5fa4fafd03f736e1
Alam Arias <Alam.GBC@gmail.com>
2017-05-23 08:30:24 -0400

    NL: spellcheck with NL dict

--- _sh/aspell.sh
+++ _sh/aspell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LANGDICT=en
+LANGDICT=nl
 cp _aspell/PSO2.dict.BASE /tmp/PSO2.dict
 find . -name '*_name.csv' -or -name 'ui_charamake_parts.csv' -print0|PYTHONIOENCODING=utf-8 xargs -0 _py/aspell.py|sed -e 's/[ \t][ \t]*/\n/g' -e 's/\!//g' -e "s/'//g"|./_tools/sortuniq.sh|grep -v -E -f _aspell/reject.dict|strings -n 1 > /tmp/PSO2.dict.DYN
 cat _aspell/PSO2.dict.BASE /tmp/PSO2.dict.DYN _aspell/PSO2.*.dict|strings -n 1 > /tmp/PSO2.dict
