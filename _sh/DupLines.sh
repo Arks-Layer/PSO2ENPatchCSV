41ca657789e9406fcecce8d05d616c6507fb98b3
Alam Arias <Alam.GBC@gmail.com>
2017-09-07 11:25:31 -0400

    Scripts: have getDupLines.py work on the JP folder

--- _sh/DupLines.sh
+++ _sh/DupLines.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-_py/getDupLines.py . > /tmp/DupLines.json
+# this assume outputPO.sh had ran
+_py/getDupLines.py /tmp/PSO2ENPatchCSV.outputPO/JP/ > /tmp/DupLines.json
 _py/checkDupLines.py . /tmp/DupLines.json
