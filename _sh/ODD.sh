2b2e962e0ea44b67bda18a55803d2ee10185628a
Alam Arias <Alam.GBC@gmail.com>
2019-01-17 14:07:35 -0500

    Scripts: Check for ODD strings in NPC names

--- _sh/ODD.sh
+++ _sh/ODD.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -name "*.csv" -not -path "*/Files/*" -not -path "*/.git/*" -print0|_tools/mp.sh --no-keep-order -0 _py/ODD.py
+find . -name "actor_name.csv" -not -path "*/Files/*" -not -path "*/.git/*" -print0|_tools/mp.sh --no-keep-order -0 _py/ODD.py
