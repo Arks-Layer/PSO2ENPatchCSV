3db29ee8b993682dcd3e6da8fde7e2c46faf8677
Alam Arias <Alam.GBC+git@gmail.com>
2019-08-24 23:32:42 -0400

    Scripts: add file exists and size text to listaspell.sh

--- _sh/listaspell.sh
+++ _sh/listaspell.sh
@@ -1,5 +1,8 @@
-#/bin/sh
+#/bin/bash
 set -euo pipefail
-cat /tmp/dict.txt
-find . -type f -name "*.csv" -not -path "*/Files/*.csv" -print0|_tools/mp.sh -0 fgrep -w -f /tmp/dict.txt
-find . -maxdepth 1 -name "aspell.nofail"|egrep '.*' > /dev/null || exit `wc -l /tmp/dict.txt|cut -f1 --d " "`
+if [[ -f /tmp/dict.txt && -s /tmp/dict.txt ]]
+then
+  cat /tmp/dict.txt
+  find . -type f -name "*.csv" -not -path "*/Files/*.csv" -print0|_tools/mp.sh -0 fgrep -w -f /tmp/dict.txt
+  find . -maxdepth 1 -name "aspell.nofail"|egrep '.*' > /dev/null || exit `wc -l /tmp/dict.txt|cut -f1 --d " "`
+fi
