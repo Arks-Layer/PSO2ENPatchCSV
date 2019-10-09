60b541ed18ab03ce831ba04ac831b100883e1443
Alam Arias <Alam.GBC+git@gmail.com>
2019-10-09 13:56:50 -0400

    Scripts: filter out commands for aspell

--- _sh/aspell.sh
+++ _sh/aspell.sh
@@ -8,7 +8,7 @@ if [ -e aspell.disabled ]; then
 	echo Spell Checking disabled
 	exit 0
 fi
-PYTHONIOENCODING=utf-8 find . -name "*.csv" -not -name "smut_filter.csv" -not -name "*_BACKUP_*.csv" -not -name "*_BASE_*.csv" -not -name "*_REMOTE_*.csv" -not -path "./.git/*" -not -path "./Files/*" -not -name "oa_tanabata.csv" -print0|xargs -0 _py/aspell.py|tee /dev/null|strings -n 2 -e S|tee /tmp/strings.txt|_tools/mp.sh --round-robin --pipe aspell pipe --personal=/tmp/PSO2.dict --mode=none --encoding utf-8 --lang=$LANGDICT|strings -n 2 -e S|fgrep -v -e "*" -e "spell "
+PYTHONIOENCODING=utf-8 find . -name "*.csv" -not -name "smut_filter.csv" -not -name "*_BACKUP_*.csv" -not -name "*_BASE_*.csv" -not -name "*_REMOTE_*.csv" -not -path "./.git/*" -not -path "./Files/*" -not -name "oa_tanabata.csv" -print0|xargs -0 _py/aspell.py|_py/filtercmd.py|tee /dev/null|strings -n 2 -e S|tee /tmp/strings.txt|_tools/mp.sh --round-robin --pipe aspell pipe --personal=/tmp/PSO2.dict --mode=none --encoding utf-8 --lang=$LANGDICT|strings -n 2 -e S|fgrep -v -e "*" -e "spell "
 rc=$?
 if [ -e aspell.warning ]; then
 	if [ $rc -ne 0 ]; then
