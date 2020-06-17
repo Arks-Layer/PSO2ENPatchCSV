3d0879be8662edc0116967eb5406c38146b50df9
focuslite <formelodyne@gmail.com>
2020-06-17 15:45:54 +0900

    Update Fonts.py
    Fonts update complete

--- _py/Fonts.py
+++ _py/Fonts.py
@@ -1123,7 +1123,7 @@ def setupfont():
 	# Hangul Syllables  | https://unicode.org/charts/PDF/UAC00.pdf
 	cmap.update(range(0xAC00,0xD7A3))
 	# Hangul Jamo  | https://en.wikipedia.org/wiki/Hangul_Jamo_(Unicode_block)
-	cmap.update(range(0x1100,0x11FF))
+	cmap.update(range(0x3130,0x318F))
 	cmap.remove(0xBDC1)
 	return cmap
 
