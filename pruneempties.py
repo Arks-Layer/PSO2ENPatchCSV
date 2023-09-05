8619279b1f695eb25b25a852c5704ac63828f8bc
DabirSA <real.manjackson@gmail.com>
2023-09-05 13:36:44 +0100

    Prune dummy files as well

--- pruneempties.py
+++ pruneempties.py
@@ -40,3 +40,38 @@ for line in flines:
         flist.write(line)
         
 flist.close()
+
+# Get list of dummies
+dummies = os.listdir("Dummy")
+
+print("Before proceeding, please check that " +
+      "no files in /Dummy are being used.")
+print("Press enter to confirm you have made this check, " +
+      "or press CTRL + C to cancel.")
+input()
+
+# Get filelist data
+flist = open("filelist.txt", "r")
+flines = flist.readlines()
+flist.close()
+
+flist = open("filelist.txt", "w")
+
+# Iterate over lines in filelist
+for line in flines:
+    dummy = "no"
+    
+    for file in dummies:
+        text = file.strip(".csv") + ".text"
+        
+        if ("," + text) in line:
+            dummy = "yes"
+            break
+
+    if dummy == "yes":
+        print("Removed dummy file {name}"
+              .format(name = text))
+    else:
+        flist.write(line)
+        
+flist.close()
