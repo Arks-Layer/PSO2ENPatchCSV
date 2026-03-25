d437e7985ea454a3e129aa6794de2422426c7274
gesoripro <formelodyne@gmail.com>
2026-03-26 00:16:54 +0900

    KO: Fix Episode 1 story text and outputPO checks

--- _py/outputPO.py
+++ _py/outputPO.py
@@ -48,76 +48,94 @@ err = 0
 if sys.argv[1] == "en":
 	ENcheckForce = True
 itemlist = ["ui_accessories_text", "ui_charamake_parts"]
+
+
+def readcsv(path):
+	with codecs.open(path, encoding="utf-8") as handle:
+		return list(csv.reader(handle, strict=True))
+
+
 for i in sys.argv[2:]:
 	w = i.replace("JP/", "WC/")
 	e = i.replace("JP/", "EN/")
-	with codecs.open(i, encoding="utf-8") as JP:
-		ENcheck = ENcheckForce
-		JPcheck = False
-		JPCSV = list(csv.reader(JP, strict=True))
-		ENCSV = list(csv.reader(codecs.open(e, encoding="utf-8"), strict=True))
-		WCCSV = list(csv.reader(codecs.open(w, encoding="utf-8"), strict=True))
-		basename = os.path.splitext(os.path.basename(i))[0]
-		if JPCSV == WCCSV:
-			JPcheck = True
+	ENcheck = ENcheckForce
+	JPcheck = False
+	basename = os.path.splitext(os.path.basename(i))[0]
+	skip = basename in itemlist
+	JPCSV = readcsv(i)
+	try:
+		ENCSV = readcsv(e)
+	except FileNotFoundError:
+		ENCSV = JPCSV
+	except csv.Error:
+		if not skip:
+			raise
+		ENCSV = JPCSV
+	try:
+		WCCSV = readcsv(w)
+	except FileNotFoundError:
+		WCCSV = JPCSV
+	except csv.Error:
+		if not skip:
+			raise
+		WCCSV = JPCSV
+	if JPCSV == WCCSV:
+		JPcheck = True
 		#if ENCSV == WCCSV:
 		#	ENcheck = True
 		#if ENcheck and JPcheck:
 		#	continue
-		skip = False
-		if basename in itemlist:
-			skip = True
-		for x, row in enumerate(JPCSV):
-			ID = row[0]
-			POID = poformat(ID)
-			JP = JPCSV[x][1][1:-1]
-			POJP = poformat(JP)
-			EN = ENCSV[x][1][1:-1]
-			POEN = poformat(EN)
-			WC = WCCSV[x][1][1:-1]
-			POWC = poformat(WC)
-			POWCF = POWC.replace("　", " ")
-			#white-space
-			print("")
-			# #  translator-comments
-			# #. extracted-comments
-			# #: reference…
-			print("#: {}:{}".format(basename, POID))
-			# #, flag…
-			print("#, no-c-format")
-			# #| msgctxt previous-context
-			print("#| msgctxt \"{}:{}\"".format(basename, POID))
-			# #| msgid previous-untranslated-string
-			print("#| msgid \"{}\"".format(POJP))
-			# msgctxt context
-			print("msgctxt \"{}:{}\"".format(basename, ID))
-			if JPcheck:
-				#msgid untranslated-string
-				print("msgid \"{}\"".format(POJP))
-				#msgstr translated-string
-				print("msgstr \"\"")
-			elif ENcheck and JP != WC:
-				#msgid untranslated-string
-				print("msgid \"{}\"".format(POJP))
-				#msgstr translated-string
-				print("msgstr \"{}\"".format(POWC))
-			elif JP == WC or JP == POWCF:
-				#msgid untranslated-string
-				print("msgid \"{}\"".format(POEN))
-				#msgstr translated-string
-				print("msgstr \"\"")
-			elif badcheck(EN, WC, POWCF, POEN, POWC):
-				#msgid untranslated-string
-				print("msgid \"{}\"".format(POEN))
-				#msgstr translated-string
-				print("msgstr \"\"")
-				if not skip:
-					print("{}:{} {} \"{}\" =~ \"{}\"".format(basename, x + 1, ID, WC, JP), file=sys.stderr)
-					err = 1
-			else:
-				#msgid untranslated-string
-				print("msgid \"{}\"".format(POEN))
-				#msgstr translated-string
-				print("msgstr \"{}\"".format(POWC))
+	for x, row in enumerate(JPCSV):
+		ID = row[0]
+		POID = poformat(ID)
+		JP = JPCSV[x][1][1:-1]
+		POJP = poformat(JP)
+		EN = ENCSV[x][1][1:-1]
+		POEN = poformat(EN)
+		WC = WCCSV[x][1][1:-1]
+		POWC = poformat(WC)
+		POWCF = POWC.replace("　", " ")
+		#white-space
+		print("")
+		# #  translator-comments
+		# #. extracted-comments
+		# #: reference…
+		print("#: {}:{}".format(basename, POID))
+		# #, flag…
+		print("#, no-c-format")
+		# #| msgctxt previous-context
+		print("#| msgctxt \"{}:{}\"".format(basename, POID))
+		# #| msgid previous-untranslated-string
+		print("#| msgid \"{}\"".format(POJP))
+		# msgctxt context
+		print("msgctxt \"{}:{}\"".format(basename, ID))
+		if JPcheck:
+			#msgid untranslated-string
+			print("msgid \"{}\"".format(POJP))
+			#msgstr translated-string
+			print("msgstr \"\"")
+		elif ENcheck and JP != WC:
+			#msgid untranslated-string
+			print("msgid \"{}\"".format(POJP))
+			#msgstr translated-string
+			print("msgstr \"{}\"".format(POWC))
+		elif JP == WC or JP == POWCF:
+			#msgid untranslated-string
+			print("msgid \"{}\"".format(POEN))
+			#msgstr translated-string
+			print("msgstr \"\"")
+		elif badcheck(EN, WC, POWCF, POEN, POWC):
+			#msgid untranslated-string
+			print("msgid \"{}\"".format(POEN))
+			#msgstr translated-string
+			print("msgstr \"\"")
+			if not skip:
+				print("{}:{} {} \"{}\" =~ \"{}\"".format(basename, x + 1, ID, WC, JP), file=sys.stderr)
+				err = 1
+		else:
+			#msgid untranslated-string
+			print("msgid \"{}\"".format(POEN))
+			#msgstr translated-string
+			print("msgstr \"{}\"".format(POWC))
 
 sys.exit(err)
